4403e86332a6dd09f9fd217c6faeb844d43f9d95
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-07-30 06:23:51 +0200

    more to the right

--- btcdonate/btcdonate.js
+++ btcdonate/btcdonate.js
@@ -52,7 +52,7 @@ var btcdonate = function(options){
     $(this).append($bubble);
 
     var bubble_offset_vertical = ($bubble.height() + 25) * -1;
-    var bubble_offset_horizontal = (($bubble.width() - $(trigger).width()) / 2) * -1 + 7;
+    var bubble_offset_horizontal = (($bubble.width() - $(trigger).width()) / 2) * -1 + 12;
 
     // Set the mouseover and mouseout on both elements
     $([trigger, $bubble.get(0)]).mouseover(function () {
